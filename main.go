25236b1422524f557b707871e7654886ca6a8b28
lordxarus <lordxarus@gmail.com>
2025-09-14 20:01:06 -0400

    feat: Build MVP CLI, write directly to stdout, enforce Colorizer contract
        API refactor: This commit unexports colorizeRune to enforce the use of
    Colorizer.Colorize(Reader, Writer). Colorize manages the IO, and is the contact
    point with catbow for a consumer.
    
        This commit defines an interface called Cleanupper. Move Cleanup() from
    ColorStrategy to Cleanupper.
    
        This commit creates a minimal CLI in main.go. CLI will interact
    with the public facing catbow API to configure and orchestrate
    a Colorizer. Some things of note:
    
            - CLI calls and prints ColorStrategy.Cleanup() to stdout when
            the Colorizer exits on an error or EOF. This properly cleans up
            SGR attributes.
    
            - CLI includes a function that creates a mock reader, string
            is supplied by shelling out to generate.py. Enable with -gen
            flag
    
        Alongside this the bufio.ReaderWriter has been traded out in favor of
    an unbuffered io.Writer. The buffered model is not performant because a flush
    after every write was required to guarantee output was displayed.
    As a result this commit had to utilize a different method for reading the runes
    which is achieved through a bufio.Scanner with a bufio.RuneScanner Split function

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"slices"
+	"strings"
+
+	"github.com/lordxarus/catbow/catbow"
+)
+
+func newMockReader() *bufio.Reader {
+	cmd := exec.Command("./generate_text.py")
+	text, err := cmd.Output()
+	println(len(text))
+	if err != nil {
+		fmt.Println(err)
+	}
+	r := bufio.NewReader(strings.NewReader(string(text)))
+	return r
+}
+
+// simplest runner to test Cleanup()
+func main() {
+
+	var r *bufio.Reader
+
+	if slices.Contains(os.Args, "-gen") {
+		r = newMockReader()
+	} else {
+		r = bufio.NewReader(os.Stdin)
+	}
+	w := io.Writer(os.Stdout)
+	colorizer := catbow.NewColorizer(catbow.NewRainbowStrategyDefaultOpts())
+	err := colorizer.Colorize(r, w)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	/* this will be replaced by
+	AnsiFormatter satisfies Formatter  interface
+	colFmt := AnsiFormatter()
+	if colFmt.(catbow.Cleanupper) {
+		w.colFmt.Cleanup()
+	}
+	*/
+	if cleaner, ok := colorizer.Strategy.(catbow.Cleanupper); ok {
+		_, err := w.Write([]byte(cleaner.Cleanup()))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+	}
+}
